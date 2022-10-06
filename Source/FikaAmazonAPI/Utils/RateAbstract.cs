56e4de7c900b02d900fd050cd0077d84ab425b36
Renzo <renzo.fraschini@gmail.com>
2022-10-06 16:24:28 +0100

    quick hack to abstract the rate limit to a file for testing

---
+++ Source/FikaAmazonAPI/Utils/RateAbstract.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FikaAmazonAPI.Utils
+{
+    public class RateAbstract
+    {
+        public DateTime LastRun { get; set; } = DateTime.UtcNow;
+        public int RequestSent { get; set; } = 0;
+    }
+}
